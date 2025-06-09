dea27ff878e8a347af06ee3c5180dd200fb79861
Abbas Rizvi <abbas.rizvi@nomadrail.com>
2025-06-09 12:07:24 +0200

    fix(tests): add debug method to logger mock in claims validation tests

--- jest.polyfills.js
+++ jest.polyfills.js
@@ -13,3 +13,54 @@ if (!global.URLSearchParams) {
   const { URLSearchParams } = require('url');
   global.URLSearchParams = URLSearchParams;
 }
+
+// Add ReadableStream, WritableStream, and TransformStream polyfills
+const { ReadableStream, WritableStream, TransformStream } = require('web-streams-polyfill');
+global.ReadableStream = ReadableStream;
+global.WritableStream = WritableStream;
+global.TransformStream = TransformStream;
+
+// Add MessagePort, MessageChannel, and BroadcastChannel polyfills
+class MessagePort {
+  constructor() {
+    this.onmessage = null;
+  }
+  postMessage() {}
+  start() {}
+  close() {}
+  addEventListener() {}
+  removeEventListener() {}
+  dispatchEvent() { return true; }
+}
+
+class MessageChannel {
+  constructor() {
+    this.port1 = new MessagePort();
+    this.port2 = new MessagePort();
+  }
+}
+
+class BroadcastChannel {
+  constructor(name) {
+    this.name = name;
+    this.onmessage = null;
+  }
+  postMessage() {}
+  close() {}
+  addEventListener() {}
+  removeEventListener() {}
+  dispatchEvent() { return true; }
+}
+
+global.MessagePort = MessagePort;
+global.MessageChannel = MessageChannel;
+global.BroadcastChannel = BroadcastChannel;
+
+// Add Request and Response polyfills
+const { Request, Response, Headers, FormData, File, Blob } = require('undici');
+global.Request = Request;
+global.Response = Response;
+global.Headers = Headers;
+global.FormData = FormData;
+global.File = File;
+global.Blob = Blob;
